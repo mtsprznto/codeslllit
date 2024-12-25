66ad8a8fb53d5671ee2aafe0dbc5741c6404aa60
Matias <lit.io30303@gmail.com>
2024-12-24 21:49:10 -0300

    constantes

--- constantes.py
+++ constantes.py
@@ -1,7 +1,7 @@
 
 SPOTIFY_LINK = "https://open.spotify.com/intl-es/artist/0oL7mgftYGFe5wWq0b9G4g"
-SOUNDCLOUD_LINK = "https://soundcloud.com/lllit_3"
-BANDCAMP_LINK = "https://lllit3.bandcamp.com/"
+SOUNDCLOUD_LINK = "https://soundcloud.com/lllit_3/sets/silencio-esperado"
+BANDCAMP_LINK = "https://lllit3.bandcamp.com/album/silencio-esperado"
 YOUTUBE_LINK = ""
 
 CORREO = "lit.io30303@gmail.com"
